a27620207824c869971b90d836eefe17d4b7aaae
jyj <131592978+pockyjx@users.noreply.github.com>
2023-09-18 17:32:03 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		if(a > 0 && b < 10) System.out.println(a+b);
+
+	}
+
+}

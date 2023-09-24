70e70ce6904a56b5499902778644a2e34cdcc2c5
jyj <0127828b@gmail.com>
2023-09-24 15:39:17 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 140 ms, Memory: 14628 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,37 @@
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=(n-i);j++) 
+				sb.append(" ");
+			
+			for(int k=1;k<=(2*i-1);k++) 
+				sb.append("*");
+	
+			sb.append("\n");
+		}
+		
+		for(int i=n-1;i>=1;i--) {
+			for(int j=1;j<=(n-i);j++) 
+				sb.append(" ");
+			
+			for(int k=1;k<=(2*i-1);k++) 
+				sb.append("*");
+			
+			sb.append("\n");
+		}
+		
+		bw.write(sb + "");
+					
+		br.close();
+		bw.close();
+	}
+}

7a910ea551cc8814ea4b385f5606f0e6a51f052c
Ye Jin <0127828b@gmail.com>
2023-10-10 15:59:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 136 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -7,17 +7,17 @@ public class Main {
 		int n = Integer.parseInt(br.readLine());
 		int min = Integer.MAX_VALUE;
 		
-		for(int i=n/3;i>=0;i--) {
+		for(int i=n/5;i>=0;i--) {
 			int num = n; int cnt = 0;
 			
 			for(int j=i;j>0;j--) {
-				num -= 3;
+				num -= 5;
 				cnt++;
 			}
 			
-			cnt += num/5;
+			cnt += num/3;
 			
-			if(num % 5 == 0) 
+			if(num % 3 == 0) 
 				min = cnt < min ? cnt : min;
 		}
 		

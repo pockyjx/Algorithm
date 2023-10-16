153e5a4dbb2881c93e43b8d85d602b23d528bd26
Ye Jin <0127828b@gmail.com>
2023-10-16 12:23:34 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 812 ms, Memory: 56844 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -4,20 +4,19 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(br.readLine());
-		Integer[][] arr = new Integer[n][2];
+		int[][] arr = new int[n][2];
 		for(int i=0;i<n;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			arr[i][0] = Integer.parseInt(st.nextToken());
 			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arr, new Comparator<Integer[]>() {
+		Arrays.sort(arr, new Comparator<int[]>() {
 			@Override
-			public int compare(Integer[] o1, Integer[] o2) {
-				if(o1[0].equals(o2[0])) {
+			public int compare(int[] o1, int[] o2) {
+				if(o1[0] == o2[0]) {
 					return o1[1] - o2[1];
 				} else {
 					return o1[0] - o2[0];
@@ -25,6 +24,7 @@ public class Main {
 			}
 		});
 		
+		StringBuilder sb = new StringBuilder();
 		for(int i=0;i<n;i++) {
 			sb.append(arr[i][0] + " " + arr[i][1]).append("\n");
 		}
@@ -33,4 +33,4 @@ public class Main {
 		br.close();
 		bw.close();
 	}
-}	
+}

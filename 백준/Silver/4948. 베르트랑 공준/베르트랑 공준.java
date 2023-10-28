879ebf1c529adf5e505484d04f0e12fb45240b47
Ye Jin <0127828b@gmail.com>
2023-10-28 12:30:36 +0900

    [Silver II] Title: 베르트랑 공준, Time: 200 ms, Memory: 16380 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,31 +1,39 @@
 import java.io.*;
+import java.util.*;
 public class Main {
+	
+	static boolean[] prime = new boolean[246913];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
+		getPrime();
 		while(true) {
 			int n = Integer.parseInt(br.readLine());
 			if(n == 0) break;
 			
 			int cnt = 0;
 			for(int i=n+1;i<=2*n;i++) {
-				boolean isPrime = true;
-				
-				for(int j=2;j<=Math.sqrt(i);j++) {
-					if(i % j == 0) {
-						isPrime = false; break;
-					}
-				}
-				if(isPrime) cnt++;
+				if(!prime[i]) cnt++;
 			}
-			sb.append(cnt + "\n");
+			sb.append(cnt).append("\n");
 		}
 		bw.write(sb + "");
 		
 		br.close();
 		bw.close();
+	}
+	
+	static void getPrime() {
+		prime[0] = prime[1] = true;
+		
+		for(int i=2;i<=Math.sqrt(prime.length);i++) {
+			if(prime[i]) continue;
+			for(int j=i*i;j<prime.length;j+=i) {
+				prime[j] = true;
+			}
+		}
 	}
 }

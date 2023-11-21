d8e39d2d98321eceb27e1f85d009330d48935ac3
Ye Jin <0127828b@gmail.com>
2023-11-21 12:57:39 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,15 +1,16 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = new int[photo.length];
+
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<name.length;i++) {
+            map.put(name[i], yearning[i]);
+        }
         
+        int[] answer = new int[photo.length];
         for(int i=0;i<photo.length;i++) {
             for(int j=0;j<photo[i].length;j++) {
-                for(int k=0;k<name.length;k++) {
-                    if(photo[i][j].equals(name[k])) {
-                        answer[i] += yearning[k];
-                    }
-                }
+                answer[i] += map.getOrDefault(photo[i][j], 0);
             }
         }
         return answer;

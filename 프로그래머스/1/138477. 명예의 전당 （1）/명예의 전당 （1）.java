7ea1aecbfb9e1624567192a6f16e312c5814e1bb
Ye Jin <0127828b@gmail.com>
2023-11-27 10:07:17 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.32 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,35 +3,14 @@ class Solution {
     public int[] solution(int k, int[] score) {
      
         int[] answer = new int[score.length];
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> q = new PriorityQueue<>();
         
-        if(k <= score.length) {
-            for(int i=0;i<k;i++) { // 1일 ~ k일
-                list.add(score[i]);
-                Collections.sort(list);
-                answer[i] = list.get(0);
-            }   
-
-            for(int i=k;i<score.length;i++) { // k일 이후
-
-                if(score[i] > list.get(0)) {
-                    list.remove(0);
-                    list.add(score[i]);
-                    Collections.sort(list);
-                    answer[i] = list.get(0);
-
-                } else {
-                    answer[i] = list.get(0);
-                }
-            }
-        } else { // k > score.length
-            for(int i=0;i<score.length;i++) {
-                list.add(score[i]);
-                Collections.sort(list);
-                answer[i] = list.get(0);
-            }
+        for(int i=0;i<score.length;i++) {
+            q.add(score[i]);
+            if(q.size() > k) q.poll();
+            answer[i] = q.peek();
         }
-        
+
         return answer;
     }
 }

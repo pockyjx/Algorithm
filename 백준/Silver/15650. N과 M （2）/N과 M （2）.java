6a71c64f47b75269f6e38a7133213a0e27bd9ce5
Ye Jin <0127828b@gmail.com>
2023-11-29 18:22:34 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	
+	static int n, m;
+//	static boolean[] isUsed;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+//		isUsed = new boolean[n+1];
+		arr = new int[m];
+		
+		dfs(0, 1);
+		bw.write(sb + "");
+		
+		br.close();
+		bw.close();
+	}
+	
+	static void dfs(int depth, int num) {
+		if(depth == m) {
+			for(int i=0;i<m;i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=num;i<=n;i++) {
+			arr[depth] = i;
+			dfs(depth+1, i+1);
+		}
+	}
+}

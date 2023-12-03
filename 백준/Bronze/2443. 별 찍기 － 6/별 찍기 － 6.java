db42edf07c06f5c1aa529d75e68d97e3f8f06e39
Ye Jin <0127828b@gmail.com>
2023-12-03 22:23:29 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 132 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i=n;i>0;i--) {
+			
+			for(int j=0;j<n-i;j++) {
+				sb.append(' ');
+			}
+			
+			for(int k=0;k<2*i-1;k++) {
+				sb.append('*');
+			}
+			sb.append("\n");
+		}
+		bw.write(sb + "");
+		
+		br.close();
+		bw.close();
+	}
+}

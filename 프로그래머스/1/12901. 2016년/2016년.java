6da9410ed131efc22a97a48dfd9472bf60b99aea
Ye Jin <0127828b@gmail.com>
2023-12-05 22:23:00 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,25 +1,11 @@
 class Solution {
     public String solution(int a, int b) {
         String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
         int total = 0;
-        for(int i=1; i<a;i++) {
-            switch(i) {
-                case 1 :
-                case 3 :
-                case 5 : 
-                case 7 :
-                case 8 :
-                case 10 : 
-                case 12 : total += 31; break;
-                
-                case 4 :
-                case 6 : 
-                case 9 : 
-                case 11 : total += 30; break;
-                    
-                default : total += 29;
-            }
+        for(int i=0; i<a-1;i++) {
+            total += date[i];
         }
         total += b - 1;
     

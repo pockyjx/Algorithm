a9b941935079ca1e30ef1ec015fa8a540f514b31
Ye Jin <0127828b@gmail.com>
2023-12-15 17:57:46 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -24,8 +24,8 @@ public class Main {
 		dp[0][1] = cost[0][1];
 		dp[0][2] = cost[0][2];
 		
-		int result = Math.min(paint(n-1, 0), 
-								Math.min(paint(n-1, 1), paint(n-1, 2)));
+		paint(n);
+		int result = Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
 		
 		bw.write(result + "");
 		
@@ -33,22 +33,11 @@ public class Main {
 		bw.close();
 	}
 	
-	static int paint(int n, int color) {
-		
-		if(dp[n][color] == 0) {
-			// 빨강
-			if(color == 0)
-				dp[n][color] = Math.min(paint(n-1, 1) , paint(n-1, 2)) + cost[n][color];
-			
-			// 초록
-			else if(color ==  1)
-				dp[n][color] = Math.min(paint(n-1, 0), paint(n-1, 2)) + cost[n][color];
-			
-			// 파랑
-			else
-				dp[n][color] = Math.min(paint(n-1, 0), paint(n-1, 1)) + cost[n][color];
+	static void paint(int n) {
+		for(int i=1;i<n;i++) {
+			dp[i][0] = cost[i][0] + Math.min(dp[i-1][1], dp[i-1][2]); // 빨
+			dp[i][1] = cost[i][1] + Math.min(dp[i-1][0], dp[i-1][2]); // 초
+			dp[i][2] = cost[i][2] + Math.min(dp[i-1][0], dp[i-1][1]); // 파
 		}
-		
-		return dp[n][color];
 	}
 }

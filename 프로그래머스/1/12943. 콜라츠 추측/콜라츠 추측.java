dd546b4a148b49932e31311b9d72ead0901325b1
Ye Jin <0127828b@gmail.com>
2023-12-17 20:33:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(long num) {
+        
+        int cnt = 0;
+        while(num != 1) {
+            num = (num % 2 == 0) ? (num / 2) : (num * 3 + 1);
+            cnt++;
+            
+            if(cnt == 500) return -1;
+        }
+        
+        return cnt;
+    }
+}

2b6cef2894492a0c38e93ed712cb8eaea54b22ce
Ye Jin <0127828b@gmail.com>
2023-12-20 18:17:35 +0900

    [level 1] Title: 3진법 뒤집기, Time: 10.42 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,22 +1,13 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        
-        Stack<Integer> s = new Stack<>();
-        
-        int cnt = 0;
+       
+        String s = ""; 
         while(n > 0) {
-            cnt++;
-            s.push(n%3);
+            s += n % 3;
             n /= 3;
         }
         
-        int answer = 0;
-        for(int i=0;i<cnt;i++) {
-            System.out.print(i + " ");
-            answer += s.pop() * Math.pow(3, i);
-        }
- 
-        return answer;
+        return Integer.parseInt(s, 3);
     } 
 }

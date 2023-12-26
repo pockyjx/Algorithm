b3a02f233fc252256406e06b6880173e38bd3674
Ye Jin <0127828b@gmail.com>
2023-12-26 11:09:00 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 10.46 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -4,9 +4,10 @@ class Solution {
 
         for(int i=0;i<answer.length;i++) {
             int idx = 10000;
-            for(int j=0;j<i;j++) {
-                if(s.charAt(j) == (s.charAt(i))) 
-                    idx = Math.min(idx, i-j);
+            for(int j=i-1;j>=0;j--) {
+                if(s.charAt(j) == (s.charAt(i))) {
+                    idx = i-j; break;
+                }
             }
             answer[i] = (idx != 10000)? idx : -1;
         }

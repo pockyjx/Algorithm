6e346c404b26a36657a805d0d1c94dc21ae94ab2
Ye Jin <0127828b@gmail.com>
2023-12-27 11:13:46 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,19 +2,12 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        int[] tmp;
+
         for(int i=0;i<commands.length;i++) {
             int start = commands[i][0]-1;
-            int end = commands[i][1]-1;
+            int end = commands[i][1];
             
-            tmp = new int[end-start+1];
-            int idx = 0;
-            for(int j=start;j<=end;j++) {
-                tmp[idx] = array[j];
-                idx++;
-            }
-            
+            int[] tmp = Arrays.copyOfRange(array, start, end);
             Arrays.sort(tmp);
             answer[i] = tmp[commands[i][2]-1];
         }

7300e1c3657c2093396bfb845914febb156ddca9
Ye Jin <0127828b@gmail.com>
2023-12-29 17:05:16 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,28 +1,24 @@
 class Solution {
-    
-    static int cnt = 0;
-    static int sum = 0;
-    
     public int solution(int[] nums) {
         
-        dfs(0, 0, nums);
-        return cnt;
-    }
-    
-    static void dfs(int depth, int idx,int[] nums) {
+        int cnt = 0;
         
-        if(depth == 3) {
-            for(int i=2;i<=(int)Math.sqrt(sum);i++) {
-                if(sum % i == 0) return;
+        for(int i=0;i<nums.length-2;i++) {
+            for(int j=i+1;j<nums.length-1;j++) {
+                for(int k=j+1;k<nums.length;k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    boolean isPrime = true;
+                    
+                    for(int l=2;l<=(int)Math.sqrt(sum);l++) {
+                        if(sum % l == 0) {
+                            isPrime = false; break;
+                        }
+                    }
+                    if(isPrime) cnt++;
+                }
             }
-            cnt++; return;
         }
-        
-        for(int i=idx;i<nums.length;i++) {
-            sum += nums[i];
-            dfs(depth+1, i+1, nums);
-            sum -= nums[i];
-        }
-        return;
+ 
+        return cnt;
     }
 }

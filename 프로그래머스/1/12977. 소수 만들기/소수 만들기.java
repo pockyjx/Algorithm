849e6ec494b5c7e83c8dce89673c42bbb5c78260
Ye Jin <0127828b@gmail.com>
2023-12-29 16:51:22 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    static int cnt = 0;
+    static int sum = 0;
+    
+    public int solution(int[] nums) {
+        
+        dfs(0, 0, nums);
+        return cnt;
+    }
+    
+    static void dfs(int depth, int idx,int[] nums) {
+        
+        if(depth == 3) {
+            for(int i=2;i<=(int)Math.sqrt(sum);i++) {
+                if(sum % i == 0) return;
+            }
+            cnt++; return;
+        }
+        
+        for(int i=idx;i<nums.length;i++) {
+            sum += nums[i];
+            dfs(depth+1, i+1, nums);
+            sum -= nums[i];
+        }
+    }
+}

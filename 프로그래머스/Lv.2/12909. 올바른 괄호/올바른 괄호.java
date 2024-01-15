230921672b70b9615747487f12e28214b4027660
Ye Jin <0127828b@gmail.com>
2024-01-15 16:09:13 +0900

    [level Lv.2] Title: 올바른 괄호, Time: 19.24 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+    
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            
+            if(ch == '(') stack.push(ch);
+            else {
+                if(stack.empty()) return false;
+                else stack.pop();
+            }
+        }
+        
+        return (stack.size() == 0) ? true : false;
+    }
+}

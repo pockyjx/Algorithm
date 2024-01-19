caff288dc33d5255a98012f038984adb924cb27b
Ye Jin <0127828b@gmail.com>
2024-01-19 10:06:49 +0900

    [level 2] Title: 짝지어 제거하기, Time: 89.63 ms, Memory: 58.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+    
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            
+            if(stack.empty() || stack.peek() != ch) stack.push(ch);
+            else stack.pop();
+        }
+    
+        return (stack.size() == 0) ? 1 : 0;
+    }
+}

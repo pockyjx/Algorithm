3c2cea706767b7501fd9a6ca8a05649b2dacbea1
Ye Jin <0127828b@gmail.com>
2024-01-24 10:33:32 +0900

    [level 2] Title: H-Index, Time: 0.56 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int answer = 0;
+        int n = citations.length;
+        
+         for(int i=0;i<n;i++) {
+             answer = Math.max(answer, Math.min(citations[i], n-i));
+         }
+        
+        return answer;
+    }
+}

f0f18275648d479ef6f6aedf37a8ba4faf06126a
Ye Jin <0127828b@gmail.com>
2024-02-07 12:51:29 +0900

    [Gold IV] Title: 불!, Time: 552 ms, Memory: 79356 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,120 +1,93 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.io.*;
+import java.util.*;
 public class Main {
 
-    static int row;
-    static int column;
-    static char[][] map;
-    static int[][] time;
-    static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, 1, -1};
-    static boolean isEscape;
-    static int ans;
-    static Queue<int[]> q = new LinkedList<>();
-    static Queue<int[]> fire = new LinkedList<>();
+    public static class Node {
+        int x, y;
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        row = Integer.valueOf(input[0]);
-        column = Integer.valueOf(input[1]);
-        map = new char[row][column];
-        time = new int[row][column];
-        isEscape = false;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < row; i++) {
-            String info = br.readLine();
-            for (int j = 0; j < column; j++) {
-                map[i][j] = info.charAt(j);
+        int[][] move = new int[r][c];
+        int[][] fire = new int[r][c];
 
-                if (map[i][j] == 'J') {
+        Queue<Node> moveQ = new LinkedList<>();
+        Queue<Node> fireQ = new LinkedList<>();
 
-                    time[i][j] = 1;
+        for(int i=0;i<r;i++) {
+            String s = br.readLine();
+            for(int j=0;j<c;j++) {
+                char ch = s.charAt(j);
 
-                    // 처음 배치부터 경계선에 있는 경우, 1분으로 바로 탈출 할 수 있다.
-                    if (i == 0 || i == row - 1 || j == 0 || j == column - 1) {
-                        System.out.println(1);
-                        return;
-                    }
-                    q.offer(new int[]{i, j});
+                if(ch == 'J') {
+                    move[i][j] = 1;
+                    moveQ.offer(new Node(i, j));
                 }
-                if (map[i][j] == 'F') {
-                    fire.offer(new int[]{i, j});
+                else if(ch == 'F') {
+                    fire[i][j] = 1;
+                    fireQ.offer(new Node(i, j));
                 }
-            }
-
-        }
-
-		// fire 에 담아둔 위치 정보를 q에 모은다.
-        while(!fire.isEmpty()){
-            q.offer(fire.poll());
-        }
-
-        bfs();
-
-        // bfs 이후, 탈출 불가능 판단이면 IMPOSSIBLE 출력
-
-        if (!isEscape) {
-            System.out.println("IMPOSSIBLE");
-        } else {
-            System.out.println(ans);
-        }
-
-
-    }
-
-    static boolean checkNearFire(int r, int c) {
-        // 경계선에 도달했을 때 인접한 곳에 불이 있는지 확인
-        for (int i = 0; i <= 3; i++) {
-            int checkR = r + dr[i];
-            int checkC = c + dc[i];
-            if ((0 <= checkR && checkR < row) && (0 <= checkC && checkC < column)) {
-                if (map[checkR][checkC] == 'F') {
-                    return true;
+                else {
+                    move[i][j] = ch;
+                    fire[i][j] = ch;
                 }
             }
         }
-        return false;
-    }
 
-    static void bfs() {
-        while (!q.isEmpty()) {
-            int[] point = q.poll();
-            int r = point[0];
-            int c = point[1];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
 
-            for (int i = 0; i <= 3; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if ((0 <= nr && nr < row) && (0 <= nc && nc < column)) {
-                    if (map[r][c] == 'J' && map[nr][nc] == '.') {
-                        time[nr][nc] = time[r][c] + 1;
-                        map[nr][nc] = 'J';
-                        q.offer(new int[]{nr, nc});
+        Node tmp;
+        int px, py, nx, ny;
 
-                        if ((nr == 0 || nr == row - 1 || nc == 0 || nc == column - 1)) {
-                            ans = time[nr][nc];
+        // 불 bfs
+        while(!fireQ.isEmpty()) {
+            tmp = fireQ.poll();
+            px = tmp.x; py = tmp.y;
 
-                            if (checkNearFire(nr, nc)) {
-                                return;
-                            }
+            for(int i=0;i<4;i++) {
+                nx = px + dx[i]; ny = py + dy[i];
+                if(nx < 0 || ny < 0 || nx > r-1 || ny > c-1) continue;
+                if(fire[nx][ny] == '.') {
+                    fire[nx][ny] = fire[px][py] + 1;
+                    fireQ.offer(new Node(nx, ny));
+                }
+            }
+        }
 
-                            isEscape = true;
-                            return;
-                        }
+        // 지훈 bfs
+        while(!moveQ.isEmpty()) {
+            tmp = moveQ.poll();
+            px = tmp.x; py = tmp.y;
 
-                    } else if (map[r][c] == 'F' && (map[nr][nc] == '.' || map[nr][nc] == 'J')) {
-                        map[nr][nc] = 'F';
-                        q.offer(new int[]{nr, nc});
+            for(int i=0;i<4;i++) {
+                nx = px + dx[i]; ny = py + dy[i];
+                if(nx < 0 || ny < 0 || nx > r-1 || ny > c-1) { // 탈출
+                    System.out.println(move[px][py]); return;
+                }
+
+                if(move[nx][ny] != '#') {
+                    if(fire[nx][ny] != -1 && move[px][py] + 1 < fire[nx][ny]) {
+                        move[nx][ny] = move[px][py] + 1;
+                        fire[nx][ny] = -1;
+                        moveQ.offer(new Node(nx, ny));
                     }
                 }
             }
         }
+
+        // 탈출 실패
+        System.out.println("IMPOSSIBLE");
     }
 }

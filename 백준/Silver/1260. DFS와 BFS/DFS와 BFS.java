2f027beff2e7e3a22972316d249d9b176cf07c23
Ye Jin <0127828b@gmail.com>
2024-05-29 01:04:37 +0900

    [Silver II] Title: DFS와 BFS, Time: 364 ms, Memory: 24064 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,64 +2,64 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
+
 public class Main {
-	
-	static int[][] arr;
-	static boolean[] check;
-	static int n, m, v;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken()); // 정점
-		m = Integer.parseInt(st.nextToken()); // 간선
-		v = Integer.parseInt(st.nextToken()); // 출발 노드
-		
-		arr = new int[n+1][n+1];
-		check = new boolean[n+1];
-		
-		// 인접 행렬
-		for(int i=0;i<m;i++){
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			arr[a][b] = 1;
-			arr[b][a] = 1;
-		}
-		
-		dfs(v);
-		System.out.println();
-		
-		check = new boolean[n+1];
-		bfs(v);
-	}
-	
-	public static void dfs(int v) { // 재귀 함수
-		check[v] = true;
-		System.out.print(v + " ");
-		
-		for(int i=0;i<=n;i++) {
-			if(arr[v][i] == 1 && !check[i]) dfs(i);
-		}
-	}
-	
-	public static void bfs(int v) { // 큐
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(v);
-		check[v] = true;
-		
-		while(!q.isEmpty()) {
-			v = q.poll();
-			System.out.print(v + " ");
-			
-			for(int i=1;i<=n;i++) {
-				if(arr[v][i] == 1 && !check[i]) {
-					q.add(i);
-					check[i] = true;
-				}
-			}
-		}
-	}
+
+    static int[][] graph;
+    static boolean[] isVisit;
+    static int n, m, v;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+
+        graph = new int[n+1][n+1];
+        isVisit = new boolean[n+1];
+
+        for(int i=0;i<m;i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x][y] = graph[y][x] = 1;
+        }
+
+        dfs(v);
+
+        System.out.println();
+        isVisit = new boolean[n+1];
+
+        bfs(v);
+    }
+
+    static void dfs(int v) {
+        isVisit[v] = true;
+        System.out.print(v + " ");
+
+        for(int i=1;i<=n;i++) {
+            if(graph[v][i] == 1 && !isVisit[i]) dfs(i);
+        }
+    }
+
+    static void bfs(int v) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(v);
+        isVisit[v] = true;
+
+        while(!q.isEmpty()) {
+            int p = q.poll();
+            System.out.print(p + " ");
+
+            for(int i=1;i<=n;i++) {
+                if(graph[p][i] == 1 && !isVisit[i]) {
+                    q.offer(i);
+                    isVisit[i] = true;
+                }
+            }
+        }
+    }
 }

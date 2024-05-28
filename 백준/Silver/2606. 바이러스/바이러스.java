10bd83c713c2afca03dd8f577e58183ba36a745a
Ye Jin <0127828b@gmail.com>
2024-05-28 18:49:32 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,42 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int[][] graph;
+	static boolean[] isVisit;
+	static int n, m, cnt = 0;
 
-    static int[][] network;
-    static boolean[] isVisit;
-    static int n, m;
-    static int cnt = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
-        network = new int[n+1][n+1];
-        isVisit = new boolean[n+1];
+		graph = new int[n+1][n+1];
+		isVisit = new boolean[n+1];
 
-        m = Integer.parseInt(br.readLine());
-        for(int i=0;i<m;i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+		for(int i=1;i<=m;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-            network[a][b] = 1;
-            network[b][a] = 1;
-        }
+			graph[a][b] = graph[b][a] = 1;
+		}
 
-        dfs(1);
-        System.out.println(cnt-1);
-    }
+		bfs(1);
+		System.out.println(cnt);
+	}
 
-    static void dfs(int x) {
-        isVisit[x] = true;
-        cnt++;
+	static void bfs(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(v);
+		isVisit[v] = true;
 
-        for(int i=1;i<=n;i++) {
-            if(network[x][i] == 1 && !isVisit[i]) {
-                dfs(i);
-            }
-        }
-    }
+		while(!q.isEmpty()) {
+			int p = q.poll();
+			
+			for(int i=1;i<=n;i++) {
+				if(graph[p][i] == 1 && !isVisit[i]) {
+					q.offer(i);
+					isVisit[i] = true;
+					cnt++;
+				}
+			}
+		}
+	}
 }

b67fbce9e56cb5725b0713c2b768fa868a0566e8
Ye Jin <0127828b@gmail.com>
2024-06-02 17:46:40 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,69 +1,61 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
 
+    static int[][] graph;
+    static boolean[][] isVisit;
+    static int n, m, cnt;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[][] graph;
-    static int cnt, n, m, k;
-    static Queue<int[]> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int t = Integer.parseInt(br.readLine());
         for(int i=0;i<t;i++) {
             st = new StringTokenizer(br.readLine());
-            m = Integer.parseInt(st.nextToken()); // 가로
-            n = Integer.parseInt(st.nextToken()); // 세로
-            k = Integer.parseInt(st.nextToken());
-            cnt = 0;
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
             graph = new int[n][m];
+            isVisit = new boolean[n][m];
 
             for(int j=0;j<k;j++) {
                 st = new StringTokenizer(br.readLine());
                 int y = Integer.parseInt(st.nextToken());
                 int x = Integer.parseInt(st.nextToken());
-
                 graph[x][y] = 1;
             }
 
+            cnt = 0;
             for(int j=0;j<n;j++) {
                 for(int l=0;l<m;l++) {
-                    if(graph[j][l] == 1) {
-                        q.offer(new int[] {j, l});
-                        bfs();
+                    if(graph[j][l] == 1 && !isVisit[j][l]) {
+                        dfs(j, l);
+                        cnt++;
                     }
                 }
             }
 
-            sb.append(cnt + "\n");
+            System.out.println(cnt);
         }
-
-        System.out.println(sb);
-
     }
-    
-    static void bfs() {
-        while(!q.isEmpty()) {
-            int[] poll = q.poll();
-            int px = poll[0]; int py = poll[1];
 
-            for(int i=0;i<4;i++) {
-                int nx = px + dx[i];
-                int ny = py + dy[i];
+    static void dfs(int x, int y) {
+        for(int i=0;i<4;i++) {
+            isVisit[x][y] = true;
 
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if(graph[nx][ny] == 1) {
-                    q.offer(new int[] {nx, ny});
-                    graph[nx][ny] = 0;
-                }
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if(graph[nx][ny] == 1 && !isVisit[nx][ny]) {
+                isVisit[nx][ny] = true;
+                dfs(nx, ny);
             }
         }
-        cnt++;
     }
 }

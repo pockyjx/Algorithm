1bdb94177ee37b8de917cebec4c64098e6030baf
Ye Jin <0127828b@gmail.com>
2024-06-11 21:15:27 +0900

    [level 1] Title: 공원 산책, Time: 0.20 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.14 ms
+메모리: 69.7 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 16:18:22
+2024년 06월 11일 21:15:24
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,41 +1,40 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        
         int[] answer = new int[2];
+        
         for(int i=0;i<park.length;i++) {
-            int idx = park[i].indexOf("S");
+            int idx = park[i].indexOf('S');
             if(idx != -1) {
-                answer[0] = i; answer[1] = idx;
+                answer[0] = i;
+                answer[1] = idx;
                 break;
             }
         }
         
-        for(String s : routes) {
-            String[] route = s.split(" ");
-            String op = route[0];
-            int n = Integer.parseInt(route[1]);
+        for(int i=0;i<routes.length;i++) {
+            String[] tmp = routes[i].split(" ");
+            String op = tmp[0];
+            int n = Integer.parseInt(tmp[1]);
             
             int col = answer[0];
             int row = answer[1];
             
-            for(int i=0;i<n;i++) {
-                if(op.equals("W")) row--;
-                else if(op.equals("S")) col++;
-                else if(op.equals("E")) row++;
+            for(int j=0;j<n;j++) {
+                if(op.equals("E")) row++;
+                else if(op.equals("W")) row--;
+                else if (op.equals("S")) col++;
                 else col--;
                 
-                if(col >= 0 && col < park.length &&
-                  row >= 0 && row < park[0].length()) {
-                    
-                    if(park[col].charAt(row) == 'X') break;
-                    if(i == n-1) {
-                        answer[0] = col;
-                        answer[1] = row;
-                    }
+                if(col < 0 || row < 0 || 
+                   col >= park.length || row >= park[0].length() ||
+                  park[col].charAt(row) == 'X') break;
+                
+                if(j == n-1) {
+                    answer[0] = col;
+                    answer[1] = row;
                 }
-            } 
-            
+            }
         }
         
         return answer;

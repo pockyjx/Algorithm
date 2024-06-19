efee58cd93e36a4522cd34bffc5ceed12d54cd34
Ye Jin <0127828b@gmail.com>
2024-06-19 20:29:30 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -5,12 +5,12 @@ class Solution {
         for(int i=1;i<=(int)Math.sqrt(yellow);i++) {
             if(yellow % i != 0) continue;
             
-            int heigh = i + 2;
-            int width = yellow / i + 2;
+            int row = i + 2; // 세로
+            int col = (yellow / i) + 2; // 가로
             
-            if((width * 2 + i * 2) == brown) {
-                answer[0] = width;
-                answer[1] = heigh;
+            if((col * 2 + i * 2) == brown) {
+                answer[0] = col;
+                answer[1] = row;
                 break;
             }
         }

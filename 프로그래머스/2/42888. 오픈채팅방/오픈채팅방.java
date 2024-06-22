5eac3bf7cc9f40fb5359bdc09922982055be0116
Ye Jin <0127828b@gmail.com>
2024-06-22 23:38:46 +0900

    [level 2] Title: 오픈채팅방, Time: 82.21 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,23 +1,21 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
-        
         Map<String, String> map = new HashMap<>();
+        
         for(int i=0;i<record.length;i++) {
-            String[] split = record[i].split(" ");
+            String[] tmp = record[i].split(" ");
             
-            if(!split[0].equals("Leave")) {
-                String id = split[1];
-                String nickname = split[2];
-                map.put(id, nickname);
+            if(!tmp[0].equals("Leave")) {
+                map.put(tmp[1], tmp[2]);
             }
         }
         
         List<String> list = new ArrayList<>();
         for(int i=0;i<record.length;i++) {
-            String[] split = record[i].split(" ");
-            String action = split[0];
-            String nickname = map.get(split[1]);
+            String[] tmp = record[i].split(" ");
+            String action = tmp[0];
+            String nickname = map.get(tmp[1]);
             
             if(action.equals("Enter")) {
                 list.add(nickname + "님이 들어왔습니다.");
@@ -27,9 +25,8 @@ class Solution {
         }
         
         String[] answer = new String[list.size()];
-        int idx = 0;
-        for(String s : list) {
-            answer[idx++] = s;
+        for(int i=0;i<list.size();i++) {
+            answer[i] = list.get(i);
         }
         
         return answer;

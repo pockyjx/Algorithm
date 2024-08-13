11ac0cacdbde7d0a17742af8fe42ece2b63489b2
Ye Jin <0127828b@gmail.com>
2024-08-14 00:11:51 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 19.99 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,41 +1,39 @@
 import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        
         Map<Character, Integer> map = new HashMap<>();
+        char[] type = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
         
-        for(int i=0;i<survey.length;i++) {
-            char type = ' ';
-            int score = 0;
-            
-            switch(choices[i]) {
-                case 1 : type = survey[i].charAt(0); score = 3; break;
-                case 2 : type = survey[i].charAt(0); score = 2; break;
-                case 3 : type = survey[i].charAt(0); score = 1; break;
-                    
-                case 4 : continue;
-                    
-                case 5 : type = survey[i].charAt(1); score = 1; break;
-                case 6 : type = survey[i].charAt(1); score = 2; break;
-                case 7 : type = survey[i].charAt(1); score = 3; break;
-            }
-            
-            map.put(type, map.getOrDefault(type, 0) + score);
+        for(int i=0;i<type.length;i++) {
+            map.put(type[i], 0);
         }
         
-        String[] compare = {"RT", "CF", "JM", "AN"};
-        String answer = "";
-        
-        for(String s : compare) {
-            char first = s.charAt(0);
-            char second = s.charAt(1);
+        for(int i=0;i<survey.length;i++) {
+            int choice = choices[i];
             
-            if(!map.containsKey(first)) {
-                map.put(first, 0);
+            if(choice < 4) {
+                char ch = survey[i].charAt(0);
+                map.put(ch, map.get(ch) + (4 - choice));
+            } 
+            
+            else if (choice > 4) {
+                char ch = survey[i].charAt(1);
+                map.put(ch, map.get(ch) + (choice - 4));
             }
+        }
+        
+        String answer = "";
+        String[] mbti = {"RT", "CF", "JM", "AN"};
+        
+        for(int i=0;i<mbti.length;i++) {
+            char first = mbti[i].charAt(0);
+            char second = mbti[i].charAt(1);
             
-            if(!map.containsKey(second)) answer += first;
-            else answer += (map.get(first) >= map.get(second)) ? first : second;
+            if(map.get(first) >= map.get(second)) {
+                answer += first;
+            } else {
+                answer += second;
+            }
         }
         
         return answer;

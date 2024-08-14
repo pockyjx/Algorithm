c3e0b76161c5230293a76d1e259082630d70754a
Ye Jin <0127828b@gmail.com>
2024-08-14 17:12:18 +0900

    [level 1] Title: 덧칠하기, Time: 1.94 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        
-        int cnt = 0;
-        int end = 0;
-        
+        int cnt = 1;
+        int end = section[0] + m - 1;
+
         for(int start : section) {
-            if(start > end) {
-                cnt++;
-                end = start + m - 1;
-            }
+            if(start <= end) continue;
+            
+            cnt++;
+            end = start + m - 1;
         }
         
         return cnt;

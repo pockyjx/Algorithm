5135017bcc31ea6d4c455c604722c36206a8fac8
Yejin <0127828b@gmail.com>
2024-08-22 20:44:34 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        
-        int cnt = 0; // 정확한 수 중 일치한 개수
-        int zero = 0; // 0의 개수
+        int zero = 0, cnt = 0; 
         
         for(int i=0;i<lottos.length;i++) {
-            int tmp = lottos[i];
-            if(tmp == 0) {
+            int num = lottos[i];
+            if(num == 0) {
                 zero++; continue;
             }
             
             for(int j=0;j<win_nums.length;j++) {
-                if(tmp == win_nums[j]) {
+                if(num == win_nums[j]) {
                     cnt++; break;
                 }
             }
         }
         
         int[] answer = {cnt+zero, cnt};
-        
         for(int i=0;i<2;i++) {
             switch(answer[i]) {
                 case 6 : answer[i] = 1; break;
@@ -26,9 +23,10 @@ class Solution {
                 case 4 : answer[i] = 3; break;
                 case 3 : answer[i] = 4; break;
                 case 2 : answer[i] = 5; break;
-                default : answer[i] = 6;
+                default : answer[i] = 6; break;
             }
-        }
+        } 
+    
         return answer;
     }
 }

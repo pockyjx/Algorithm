6303497c1e1f79678e7a07c3fb00ac87628fbbec
Yejin <0127828b@gmail.com>
2024-08-26 16:30:59 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.27 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,27 +1,20 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        
         int[] answer = {50, 50, -1, -1};
         
         for(int i=0;i<wallpaper.length;i++) {
-            String s = wallpaper[i];
+            String w = wallpaper[i];
             
-            if(s.contains("#")) {
-                answer[0] = Math.min(answer[0], i);
-                answer[2] = Math.max(answer[2], i+1);
-            }
-            
-                            
-            for(int j=0;j<s.length();j++) {
-                if(s.charAt(j) == '#') {
-                    answer[1] = Math.min(answer[1], j);
-                    answer[3] = Math.max(answer[3], j+1);
+            for(int j=0;j<w.length();j++) {
+                if(w.charAt(j) == '#') {
+                    answer[0] = Math.min(i, answer[0]);
+                    answer[1] = Math.min(j, answer[1]);
+                    
+                    answer[2] = Math.max(i+1, answer[2]);
+                    answer[3] = Math.max(j+1, answer[3]);
                 }
             }
-            
-            
         }
-        
         
         return answer;
     }

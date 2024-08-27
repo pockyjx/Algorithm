2e39cdfdaac3c471d3a0bab1841a286390256184
Yejin <0127828b@gmail.com>
2024-08-28 00:05:00 +0900

    [level 1] Title: 햄버거 만들기, Time: 56.46 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,19 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
-        
         int answer = 0;
         Stack<Integer> s = new Stack<>();
         
         for(int i : ingredient) {
             s.push(i);
             
-            if(s.size() >= 4) {
-                if(s.get(s.size()-4) == 1 &&
-                   s.get(s.size()-3) == 2 &&
-                   s.get(s.size()-2) == 3 &&
-                   s.get(s.size()-1) == 1) {
-                    
+            
+            if(s.size() >= 4) { 
+                if(s.get(s.size() - 4) == 1 && s.get(s.size() - 3) == 2 &&
+                  s.get(s.size() - 2) == 3 && s.get(s.size() - 1) == 1) {
                     answer++;
                     s.pop(); s.pop(); s.pop(); s.pop();
                 }

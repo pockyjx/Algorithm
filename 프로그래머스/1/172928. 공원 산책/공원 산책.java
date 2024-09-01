4faeb37e0790893e67a1b4975bad066430d49b7f
Yejin <0127828b@gmail.com>
2024-09-01 22:04:04 +0900

    [level 1] Title: 공원 산책, Time: 0.49 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,21 +1,23 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] park, String[] routes) {
         int[] answer = new int[2];
         
-        for(int i=0;i<park.length;i++) {
-            int idx = park[i].indexOf('S');
-            if(idx != -1) {
+        for(int i = 0; i < park.length; i++) {
+            String s = park[i];
+            
+            if(s.contains("S")) {
                 answer[0] = i;
-                answer[1] = idx;
+                answer[1] = s.indexOf("S");
                 break;
             }
         }
         
         for(int i=0;i<routes.length;i++) {
-            String[] tmp = routes[i].split(" ");
-            String op = tmp[0];
-            int n = Integer.parseInt(tmp[1]);
+            String[] s = routes[i].split(" ");
+            String op = s[0];
+            int n = Integer.parseInt(s[1]);
             
             int col = answer[0];
             int row = answer[1];
@@ -23,12 +25,11 @@ class Solution {
             for(int j=0;j<n;j++) {
                 if(op.equals("E")) row++;
                 else if(op.equals("W")) row--;
-                else if (op.equals("S")) col++;
+                else if(op.equals("S")) col++;
                 else col--;
                 
-                if(col < 0 || row < 0 || 
-                   col >= park.length || row >= park[0].length() ||
-                  park[col].charAt(row) == 'X') break;
+                if(col >= park.length || row >= park[0].length() || col < 0 || row < 0
+                    || park[col].charAt(row) == 'X') break;
                 
                 if(j == n-1) {
                     answer[0] = col;

ac1009cd111613b3a3efb645d75a370571df1160
Yejin <0127828b@gmail.com>
2024-09-05 19:07:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 10.54 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,11 +3,22 @@ class Solution {
         String[] answer = new String[n];
         
         for(int i=0;i<n;i++) {
-            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            answer[i] = String.format("%"+n+"s", answer[i]);
-            answer[i] = answer[i].replace('0', ' ').replace('1', '#');
-
+            String bin1 = toBinary(arr1[i], n);
+            String bin2 = toBinary(arr2[i], n);
+            
+            StringBuilder sb = new StringBuilder();
+            for(int j=0;j<n;j++) {
+                if(bin1.charAt(j) == '1' || bin2.charAt(j) == '1') sb.append("#");
+                else sb.append(" ");
+            }
+            answer[i] = sb.toString();
         }
+        
         return answer;
+    }
+    
+    static String toBinary(int num, int len) {
+        String bin = Integer.toBinaryString(num);
+        return String.format("%"+len+"s", bin).replace(' ', '0');
     }
 }

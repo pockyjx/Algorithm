a49679dcac73615f9f1d758de6caf2a675029815
Yejin <0127828b@gmail.com>
2024-09-11 14:20:11 +0900

    [level 1] Title: 대충 만든 자판, Time: 6.45 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,29 +1,29 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        
         Map<Character, Integer> map = new HashMap<>();
-        for(String s : keymap) {
-            for(int i=0;i<s.length();i++) {
-                char ch = s.charAt(i);
-                map.put(ch, Math.min(i+1, map.getOrDefault(ch, i+1)));
+        for(String k : keymap) {
+            for(int i=0;i<k.length();i++) {
+                char ch = k.charAt(i);
+                map.put(ch, Math.min(map.getOrDefault(ch, i+1), i+1));
             }
-        }        
+        }
         
         int[] answer = new int[targets.length];
         for(int i=0;i<targets.length;i++) {
             String s = targets[i];
             
             for(int j=0;j<s.length();j++) {
-                if(map.containsKey(s.charAt(j))) {
-                    answer[i] += map.get(s.charAt(j));
-                } else {
+                char ch = s.charAt(j);
+                
+                if(map.get(ch) == null) {
                     answer[i] = -1; break;
                 }
+                
+                answer[i] += map.get(ch);
             }
         }
-            
-            
+        
         return answer;
     }
 }

de272b2ad616fa0c6eaa6c9292b267bf5c6f50e4
Yejin <0127828b@gmail.com>
2024-09-22 21:48:06 +0900

    [level 1] Title: 달리기 경주, Time: 643.08 ms, Memory: 385 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,18 +1,17 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-    
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> rank = new HashMap<>();
         for(int i=0;i<players.length;i++) {
-            map.put(players[i], i);
+            rank.put(players[i], i);
         }
         
         for(String call : callings) {
-            int ranking = map.get(call); // 추월한 선수의 원래 등수
-            map.put(call, ranking-1); // 추월
+            int ranking = rank.get(call);
+            rank.put(call, ranking-1); // 추월함
             
-            String front = players[ranking-1]; // 추월 당한 선수의 이름
-            map.put(front, ranking); // 추월 당함
+            String front = players[ranking-1]; // 원래 앞에 있던 선수
+            rank.put(front, ranking); // 추월 당함
             
             players[ranking-1] = call;
             players[ranking] = front;

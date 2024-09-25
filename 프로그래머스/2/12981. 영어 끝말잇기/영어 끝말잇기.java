03093569065a0c8065e12b6e1b0c3202feaedb68
Yejin <0127828b@gmail.com>
2024-09-25 16:19:23 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.40 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,33 +1,31 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=1;i<=n;i++) {
+            map.put(i, 0); // <순서, 횟수>
+        }
         
         int[] answer = new int[2];
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i=0;i<n;i++) {
-            map.put(i+1, 0);
-        }
+        List<String> list = new ArrayList<>();
         
         int idx = 1;
         char last = words[0].charAt(0);
-        List<String> list = new ArrayList<>();
-        
-        for(String s : words) {
-            if(s.charAt(0) != last || list.contains(s)) {
+        for(String w : words) {
+            if(w.charAt(0) != last || list.contains(w)) {
                 answer[0] = idx;
                 answer[1] = map.get(idx) + 1;
                 break;
             }
             
+            list.add(w);
+            last = w.charAt(w.length()-1);
             map.put(idx, map.get(idx)+1);
-            last = s.charAt(s.length()-1);
-            list.add(s);
             
             if(idx == n) idx = 1;
             else idx++;
         }
- 
+        
         return answer;
     }
 }

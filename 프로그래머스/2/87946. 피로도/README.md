1b8e0cc694041d78b085605e5b786563c0f1bf5f
Yejin <0127828b@gmail.com>
2024-09-30 16:46:48 +0900

    [level 2] Title: 피로도, Time: 4.07 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 0.14 ms
+메모리: 73.6 MB, 시간: 4.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 15:11:31
+2024년 09월 30일 16:46:45
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,22 @@
 class Solution {
-    
-    static boolean[] isVisit;
-    
+    static boolean[] visited;
     public int solution(int k, int[][] dungeons) {
-        
-        isVisit = new boolean[dungeons.length];
-        dfs(k, dungeons);
-        
+        visited = new boolean[dungeons.length];
         return dfs(k, dungeons);
     }
     
     static int dfs(int k, int[][] dungeons) {
-        
-       int cnt = 0;
-        
+        int cnt = 0;
         for(int i=0;i<dungeons.length;i++) {
-            if(!isVisit[i] && k >= dungeons[i][0]) {
-                isVisit[i] = true;
+            if(!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
                 k -= dungeons[i][1];
                 
                 cnt = Math.max(dfs(k, dungeons), cnt);
                 
-                isVisit[i] = false;
+                visited[i] = false;
                 k += dungeons[i][1];
-            }             
+            }
         }
         
         return Math.max(cnt(), cnt);
@@ -31,9 +24,8 @@ class Solution {
     
     static int cnt() {
         int cnt = 0;
-        
-        for (boolean v : isVisit) {
-            if (v) cnt++;
+        for(boolean v : visited) {
+            if(v) cnt++;
         }
         return cnt;
     }

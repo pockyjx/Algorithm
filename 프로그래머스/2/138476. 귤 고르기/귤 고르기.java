01b74442c27f9066e753e1c872d583b09fdc4996
Yejin <0127828b@gmail.com>
2024-10-02 10:34:07 +0900

    [level 2] Title: 귤 고르기, Time: 77.48 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,24 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
-        
+        if(k == 1) return 1;
+            
         Map<Integer, Integer> map = new HashMap<>();
         for(int t : tangerine) {
-            map.put(t, map.getOrDefault(t, 0)+1);
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
         
         List<Integer> value = new ArrayList<>(map.values());
         Collections.sort(value, Collections.reverseOrder());
         
-        int total = 0;
-        int cnt = 0;
-        
+        int answer = 0;
         for(int v : value) {
-            total += v;
-            cnt++;
+            k -= v;
+            answer++;
             
-            if(total >= k) break;
+            if(k <= 0) break;
         }
-        return cnt;
+        
+        return answer;
     }
 }

67c492a63c8c6853729c71960a17357515576408
Yejin <0127828b@gmail.com>
2024-11-06 22:16:41 +0900

    [Silver III] Title: 파도반 수열, Time: 116 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,33 +1,24 @@
 import java.io.*;
 public class Main {
-	
-	static long[] memo = new long[100];
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
 		int t = Integer.parseInt(br.readLine());
-		memo[0] = memo[1] = memo[2] = 1;
-		
+
 		for(int i=0;i<t;i++) {
-			int k = Integer.parseInt(br.readLine());
-			sb.append(dp(k)).append("\n");
+			int n = Integer.parseInt(br.readLine());
+
+			if(n < 3) {
+				System.out.println(1); continue;
+			}
+
+			long[] mem = new long[n];
+			mem[0] = mem[1] = mem[2] = 1;
+
+			for(int j=3;j<n;j++) {
+				mem[j] = mem[j-2] + mem[j-3];
+			}
+
+			System.out.println(mem[n-1]);
 		}
-		bw.write(sb + "");
-		
-		br.close();
-		bw.close();
-	}
-	
-	static long dp(int n) {
-		
-		if(memo[n-1] != 0) return memo[n-1];
-		
-		for(int i=3;i<n;i++) {
-			memo[i] = memo[i-2] + memo[i-3];
-		}
-		return memo[n-1];
 	}
 }
